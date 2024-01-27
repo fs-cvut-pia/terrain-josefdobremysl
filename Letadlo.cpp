e9c838ed0ca2458a18d51af56e7daea6d629ad45
josefdobremysl <128516711+josefdobremysl@users.noreply.github.com>
2024-01-27 01:28:41 +0100

    Add files via upload

--- Letadlo.cpp
+++ Letadlo.cpp
@@ -1,23 +1,22 @@
-#include "Letadlo.h"
+﻿#include "Letadlo.h"
 #include <cstdlib>
 #include <cmath>
 #include <fstream>
 #include <iostream>
 
 
-Letadlo::Letadlo(TerrainMap& m, std::string name_in, Point start_in, Point finish_in) : Path(m, name_in, start_in, finish_in){
+Letadlo::Letadlo(TerrainMap& m, std::string name_in, Point start_in, Point finish_in) : Path(m, name_in, start_in, finish_in) {
 }
 
-Letadlo::~Letadlo(){
+Letadlo::~Letadlo() {
 }
 
 bool Letadlo::find() {
-
-	cout << "Letadlo  --  Letadlo  -- Letadlo" << endl;
 	deque<Point> Body;
 	deque<Point> Cesta;
 	Body.push_back(start);
-	double hodnota = 500;
+	int hodnota = 500;
+
 	Matrix<double> vzdalenosti(map.nx, map.ny);
 	for (int i = 1; i < 255; i++) {
 		for (int j = 1; j < 255; j++) {
@@ -25,147 +24,113 @@ bool Letadlo::find() {
 			vzdalenosti(start.x, start.y) = 0;
 		}
 	}
-	//Matrix<Point> predchozi_pole(map.nx, map.ny);
 
 	int k;
 	int l;
 	int k_i;
 	int l_j;
-	bool B = true;
+	int n = 0;
+	cout << "letadlo" << endl;
 
-	while (B==true) {
-		Point aktualni_bod = Body[0];
-		if (!Body.empty()) {
+	while (Body.size() > 1 || n < 10) {
+		n++;
+		std::cout << "Delka fronty: " << Body.size() << std::endl;
+		Point aktualni_bod = Body[0];				// První prvek z kontejneru je aktuální bod
+		Body.pop_front();
+		k = aktualni_bod.x;					// Souřadnice aktuálního bodu 
+		l = aktualni_bod.y;
+		if (k < 3 || k >(map.nx - 3) || l < 3 || l >(map.ny - 3)) {			// Pokud je bod na kraji oblasti  
+			
+			Point aktualni_bod = Body[0];										// jako aktuální bod se vezme další bod z fronty 
 			Body.pop_front();
 		}
-
-		for (int i = -1; i < 2; i++) {
+		for (int i = -1; i < 2; i++) {				// Cyklus přes všechny sousedy aktuálního bodu 
 			for (int j = -1; j < 2; j++) {
-				//std::cout << "i" << i << "  " << "j" << j << endl;
-				k = aktualni_bod.x;
+				k = aktualni_bod.x;					// Souřadnice aktuálního bodu 
 				l = aktualni_bod.y;
-				k_i = aktualni_bod.x + i;
+				k_i = aktualni_bod.x + i;			// Souřadnice sousedního bodu
 				l_j = aktualni_bod.y + j;
-				//cout << "aktu1 " << aktualni_bod.x << aktualni_bod.y << endl;
-				
-				while (k < 3 || k >(map.nx-3) || l < 3 || l >(map.ny - 3)) {
-					Body.pop_front();
-			
-					Point aktualni_bod = Body[0];
-					//cout << "aktu2 " << aktualni_bod.x << aktualni_bod.y << endl;
-
-					k = aktualni_bod.x;
-					l = aktualni_bod.y;
-					k_i = k + i;
-					l_j = l + j;
-				}
-
-				if (k_i != start.x && l_j != start.y) {
+				//cout << "777" << endl;
+				if (k_i != start.x || l_j != start.y) {
 					double wew = (vzdalenosti(k, l) + sqrt(2));
-					if (vzdalenosti(k_i, l_j) > wew ) {	// diagonalni prvky
-
-
+					if (vzdalenosti(k_i, l_j) > wew) {									// diagonalni prvky
 						if (i != 0 && j != 0) {
-
 							vzdalenosti(k_i, l_j) = vzdalenosti(k, l) + sqrt(2);
 							Point bodN(k_i, l_j);
 							Body.push_back(bodN);
 							std::cout << k_i << "  " << l_j << "  " << vzdalenosti(k_i, l_j) << endl;
 						}
 					}
+				
 					double ww = (vzdalenosti(k, l) + 1);
-					if (vzdalenosti(k_i, l_j) > ww ) {		// nediagonalni prvky
+					if (vzdalenosti(k_i, l_j) > ww) {									// nediagonalni prvky
 						if (i == 0 && j != 0 || j == 0 && i != 0) {
-							//std::cout << "899999999999999999999******************" << endl;
+			
 							vzdalenosti(k_i, l_j) = vzdalenosti(k, l) + 1;
 							Point bodN1(k_i, l_j);
-							std::cout << k_i << "  " << l_j << "  " << vzdalenosti(k_i, l_j) << endl;
-							std::cout << "Delka fronty: " << Body.size() << std::endl;
-
 							Body.push_back(bodN1);
-							//std::cout << 30 << "  " << 28 << "  " << vzdalenosti(100, 58) << endl;
 						}
 					}
-				}
-
+				
+				}				
 			}
-			
 		}
-		int ki;
-		int lj;
+	}
+	cout << "Cesta nalezena" << endl;
+		
+	// Následuje zápis cesty do souboru
 
-		Point bod_na_ceste;
-		double val;
-		if (k_i==9 && l_j == 2) {
+	int ki;
+	int lj;
 
+	Point bod_na_ceste;
+	double val;
 
-			for (Point element : Body) {
-				std::cout <<"x" << element.x << " " << "y" << element.x << " ";
+		string filename = "C:\\Users\\josef\\Desktop\\MAPA.dat";   // Otevření souboru pro zápis
+		ofstream File(filename.c_str());						   // Matice vzdáleností
+		for (int i = 0; i < 255; i++) {
+			for (int j = 0; j < 255; j++) {
+				File << vzdalenosti(i, j) << " ";
 			}
-			std::cout << "-------------------------------------------------------------------------" << endl;
-			string filename = "C:\\Users\\josef\\Desktop\\MAPA.dat";
-			// Otev�en� souboru pro z�pis
-			ofstream File(filename.c_str());
-			for (int i = 0; i < 255;i++) {
-				for (int j = 0; j < 255; j++) {
-					File << vzdalenosti(i,j) << " ";
-					
-				}
-				File << std::endl;
-			}
-			// Z�v�r souboru
-			File.close();
+			File << std::endl;
+		}
+		// Zavření souboru
+		File.close();
 
-			cout << "Soubor " << filename << " byl vytvo�en." << endl;
-			Cesta.push_front(finish);
-			Point aktualni_bod2 = finish;
-			while (true) {
-				aktualni_bod2 = Cesta[0];
-				std::cout << "x " << aktualni_bod2.x << " y " << aktualni_bod2.y << endl;
-				val = vzdalenosti(aktualni_bod2.x, aktualni_bod2.y);
-				for (int i = -1; i < 2; i++) {
-					for (int j = -1; j < 2; j++) {
-						std::cout << "i " << i << " j " << j << endl;
+		cout << "Soubor " << filename << " byl vytvo�en." << endl;
+		Cesta.push_front(finish);
+		Point aktualni_bod2 = finish;
+		while (true) {
+			aktualni_bod2 = Cesta[0];
+			val = vzdalenosti(aktualni_bod2.x, aktualni_bod2.y);
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
 
-						k_i = aktualni_bod2.x + i;
-						l_j = aktualni_bod2.y + j;
-						std::cout << vzdalenosti(k_i, l_j) << " " << val << endl;
-						if (vzdalenosti(k_i, l_j) < val) {
-							val = vzdalenosti(k_i, l_j);
-							std::cout << "****" << endl;
-							std::cout << "Delka fronty: " << Body.size() << std::endl;
+					k_i = aktualni_bod2.x + i;
+					l_j = aktualni_bod2.y + j;
+
+					if (vzdalenosti(k_i, l_j) < val) {
+						val = vzdalenosti(k_i, l_j);
+
 							ki = k_i;
 							lj = l_j;
-
 						}
 					}
 				}
 				Point bod_na_ceste(ki, lj);
-				std::cout << "xx " << bod_na_ceste.x << " yy " << bod_na_ceste.y << endl;
-				Cesta.push_front(bod_na_ceste);
-				//aktualni_bod2 = bod_na_ceste;
-			if (aktualni_bod2 == start) {
-
-				string filename = "C:\\Users\\josef\\Desktop\\Letadlo.dat";
-				// Otev�en� souboru pro z�pis
-				ofstream File(filename.c_str());
-				for (Point u : Cesta) {
-					File << u.x << " " << u.y << std::endl;
+				Cesta.push_front(bod_na_ceste);			
+				if (aktualni_bod2 == start) {
+					string filename = "C:\\Users\\josef\\Desktop\\Letadlo.dat";
+					ofstream File(filename.c_str());
+					for (Point u : Cesta) {
+						File << u.x << " " << u.y << std::endl;
+					}
+					File.close();						// Z�v�r souboru
+					cout << "Soubor " << filename << " byl vytvořen." << endl;
+					
+					std::exit(6);
 				}
-				// Z�v�r souboru
-				File.close();
-
-				cout << "Soubor " << filename << " byl vytvo�en." << endl;
-				B = false;
-				break;
-				//std::exit(6);
 			}
-			}
-		}
-	}
-	
-	return false;
-	}
-
-
+	return false;	
+}
 
--- main.cpp
+++ main.cpp
@@ -2,57 +2,44 @@
 #include "Path.h"
 #include "Letadlo.h"
 #include "Lod.h"
-
 #include <vector>
 #include <iostream>
 #include <string>
 
-
 // Include files of your path classes will need to be added here
 
-//Point read_coordinates(int argc, char* argv[], int i_option) {
-//    Point p;
-//
-//    if (argc > i_option + 1) { p.x = std::atoi(argv[i_option]); p.y = std::atoi(argv[i_option + 1]); }
-//    else throw std::runtime_error("Coordinates incorrectly specified!");
-//
-//    return p;
-//}
+Point read_coordinates(int argc, char* argv[], int i_option) {
+    Point p;
+
+    if (argc > i_option + 1) { p.x = std::atoi(argv[i_option]); p.y = std::atoi(argv[i_option + 1]); }
+    else throw std::runtime_error("Coordinates incorrectly specified!");
+
+    return p;
+}
 
 int main(int argc, char* argv[]) {
     const int nx = 256;
     const int ny = 256;
 
-    cout << "444444444444" << endl;
-
-
     std::string terrain_filename;
 
     // Load the terrain map
 
-    if (argc > 1) { terrain_filename = argv[1]; }
-    else { std::cout << "No terrain file specified!" << std::endl; }
-    terrain_filename = "terrain.dat";
+    terrain_filename = "terrain";
 
     TerrainMap m(nx, ny, terrain_filename);
 
     // Load the coordinates of the start and end points
 
-    Point start(198, 205);// = read_coordinates(argc, argv, 2);
-    Point finish(78, 17);// = read_coordinates(argc, argv, 4);
-
-    std::cout << "Start coordinates: [" << start.x << ", " << start.y << "]" << std::endl;
+    Point start(198, 205);
+    Point finish(78, 17);
 
     std::vector<Path*> paths = {
-    //    new Lod(m,"Lod",start,finish),
-        new Letadlo(m,"Letadlo",start,finish),
-    //    new Silnice(m,"Silnice",start,finish),
-    //    new Siljekt(m,"Siljekt",start,finish),
-    //    new Vlak(m,"Vlak",start,finish)*/
-    //    // Here add the list of dynamically created classes with path finding algorithms
+          //new Letadlo(m,"Letadlo",start,finish),
+          new Lod(m, "Lod", start, finish),
+        //new YourPath(m,"MyPathName",start,finish), ...
+        // Here add the list of dynamically created classes with path finding algorithms
     };
-
-    //double L = letadlo(m, "Letadlo", start, finish);
 
     for (auto& p : paths) {
         std::cout << "Path search: " << p->getName() << std::endl;
@@ -63,7 +50,6 @@ int main(int argc, char* argv[]) {
         p->saveToFile();
         delete p;
     }
-    
 
     return 0;
 }
